2b1a75dfe13836bde87cd31ea185d47b43ad65c2
jayced1 <jesseeren@gmail.com>
2025-06-20 00:02:45 +0300

    implements for the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/Cta";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className={"text-2xl underline"}>Welcome here</h1>
-      <Button> Lets start</Button>
-    </div>
+    <main>
+      <h1 className={"text-2xl underline"}>Popular Companions</h1>
+
+      <section className={"home-section"}>
+        <CompanionCard
+          id={"123"}
+          name={"Nora the Explorer"}
+          subject={"Science"}
+          topic={"The addiction curve"}
+          duration={45}
+          color={"#efaefe"}
+        />
+        <CompanionCard
+          id={"32"}
+          name={"Nora the Explorer"}
+          subject={"English"}
+          topic={"The addiction curve"}
+          duration={45}
+          color={"#ffeffe"}
+        />
+        <CompanionCard
+          id={"22"}
+          name={"Nora the Explorer"}
+          subject={"Literature"}
+          topic={"The addiction curve"}
+          duration={45}
+          color={"#fbefae"}
+        />
+      </section>
+
+      <section className={"home-section"}>
+        <CompanionsList
+          title={"Recently completed sessions"}
+          companions={recentSessions}
+          classNames={"w-2/3 max-lg:w-full"}
+        />
+        <Cta />
+      </section>
+    </main>
   );
 };
 
